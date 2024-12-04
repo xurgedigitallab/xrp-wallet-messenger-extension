9c59a100f997075bf60ddf6b359124b5201db09a
0ctoPusK <nodeforyou78@gmail.com>
2024-12-05 01:10:44 +0900

    Added functionalities and updated

--- content.js
+++ content.js
@@ -8,6 +8,7 @@ async function loadSitesConfig() {
       } else {
         console.log('Received sitesConfig from background.js');
         resolve(response.sitesConfig);
+        console.log('Sites config:', response.sitesConfig);
       }
     });
   });
@@ -18,28 +19,30 @@ function findXRPAddresses(text) {
   return text.match(xrpRegex);
 }
 
+function findFirstXRPAddressInArray(array) {
+  for (const text of array) {
+    const addresses = findXRPAddresses(text);
+    if (addresses && addresses.length > 0) {
+      return addresses[0];
+    }
+  }
+  return null;
+}
+
 function findXRPAddressInNode(node) {
   if (node) {
     // Search in attributes of the current node
     for (let i = 0; i < node.attributes.length; i++) {
       const attribute = node.attributes[i];
       const value = attribute.value;
-      if (attribute.name === 'href') {
-        if (value.includes('/profile/')) {
-          const xrpAddress = value.split('/profile/')[1];
-          console.log('Found XRP address in href:', xrpAddress);
-          return xrpAddress;
-        } else if (value.includes('/explorer/')) {
-          const xrpAddress = value.split('/explorer/')[1];
-          console.log('Found XRP address in href:', xrpAddress);
-          return xrpAddress;
-        } else {
-          const xrpAddress = findXRPAddresses(value);
-          if (xrpAddress) {
-            console.log('Found XRP address in href attribute:', attribute.name, 'with value:', value);
-            return xrpAddress[0];
-          }
-        }
+      if (attribute.name === 'href' && value.includes('/profile/')) {
+        const xrpAddress = value.split('/profile/')[1];
+        console.log('Found XRP address in href:', xrpAddress);
+        return xrpAddress;
+      } else if (attribute.name === 'href' && value.includes('/explorer/')) {
+        const xrpAddress = value.split('/explorer/')[1];
+        console.log('Found XRP address in href:', xrpAddress);
+        return xrpAddress;
       }
       const xrpAddress = findXRPAddresses(value);
       if (xrpAddress) {
@@ -121,7 +124,7 @@ function createButton(xrpAddress, buttonText) {
   // Add click event to open a new tab
   button.addEventListener('click', () => {
     const cleanAddress = xrpAddress.replace(/[^a-zA-Z0-9]/g, '');
-    const url = `https://app.textrp.io/#/user/@${cleanAddress}:synapse.textrp.io`;
+    const url = `https://app.textrp.io/#/user/@${cleanAddress}`;
     window.open(url, '_blank');
   });
 
@@ -153,15 +156,21 @@ function injectStyles() {
   document.head.appendChild(style);
 }
 
-async function insertButton(site, button, siteUrl) {
+async function insertButton(site, button, buttonAdjust) {
   // Debugging: Log all potential insert containers
-  const potentialContainers = document.querySelectorAll(site.insertSelector);
+  const potentialContainers = document.querySelector(site.insertSelector);
   console.log('Potential insert containers:', potentialContainers);
 
   let insertContainer;
   try {
-    if (site.url === 'https://dexscreener.com/xrpl') {
-      insertContainer = await waitForElement(site.insertSelector, 2000);
+    if (site.pageLoadDelay) {
+      insertContainer = await waitForElement(site.insertSelector, 1000);
+    } else if (site.secInsertSelector && site.buttonExists) {
+      if (potentialContainers) {
+        insertContainer = await waitForElement(site.insertSelector);
+      } else {
+        insertContainer = await waitForElement(site.secInsertSelector);
+      }
     } else {
       insertContainer = await waitForElement(site.insertSelector);
     }
@@ -181,23 +190,17 @@ async function insertButton(site, button, siteUrl) {
     }
   }
 
-  console.log('Found insert container:', insertContainer);
-
   // Remove the button if it already exists
   const existingButton = document.querySelector('.contact-nft-owner-button');
   if (existingButton) {
     existingButton.remove();
+    console.log('Existing button removed:', existingButton);
   }
 
   // For specific sites, insert the button using different methods.
-  if (siteUrl === 'https://xrplexplorer.com') {
-    insertContainer.insertAdjacentElement('afterend', button);
-  } else if (siteUrl === 'https://neefty.io/xrpl') {
-    insertContainer.insertAdjacentElement('afterend', button);
-  } else if (siteUrl === 'https://xmagnetic.org') {
-    insertContainer.insertAdjacentElement('afterend', button);
+  if (buttonAdjust) {
+    insertContainer.insertAdjacentElement(buttonAdjust, button);
   } else {
-    // By default, insert as the last child
     insertContainer.appendChild(button);
   }
 }
@@ -225,52 +228,87 @@ function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function parseAndExecuteExpression(expression) {
+  const parts = expression.match(/(\w+\(.*?\))|(\w+)/g);
+  console.log('Expression parts:', parts);
+  let currentObject = window;
+  for (const part of parts) {
+    const methodMatch = part.match(/(\w+)\(([^)]*)\)/);
+    if (methodMatch) {
+      const methodName = methodMatch[1];
+      const args = methodMatch[2].split(',').map(arg => arg.trim().replace(/['"]/g, ''));
+      currentObject = currentObject[methodName](...args);
+    } else {
+      currentObject = currentObject[part];
+    }
+  }
+  console.log('Expression executed:', expression, 'Result:', currentObject);
+  return currentObject;
+}
+
 async function insertButtonForSite(site) {
   console.log(`Checking URL: ${window.location.href}`);
 
   try {
-    const container = await waitForElement(site.selector);
-    console.log('Found Owner address container:', container);
-
+    let container = null;
+    let secContainer = null;
     let xrpAddress = null;
     let nftId = null;
-    if (site.url === 'https://app.zerpmon.world') {
-      xrpAddress = window.location.pathname.split('/').pop();
-      console.log('Extracted XRP address from URL:', xrpAddress);
-    } else if (site.url === 'https://sologenic.org') {
-      nftId = window.location.pathname.split('/').pop().split('?')[0];
-      console.log('found nftId', nftId);
+    let foundXRPAddress = null;
 
-      xrpAddress = await getXrpAddress(nftId);
-      console.log('found xrpAddress', xrpAddress);
-    } else if (site.url === 'https://fuzion-xio.com') {
-      nftId = container.href.split('/').pop();
-      console.log('Extracted nftId from href:', nftId);
-      
-      xrpAddress = await getXrpAddress(nftId);
-      console.log('found xrpAddress', xrpAddress);
-    } else if (site.url === 'https://artdept.fun') {
-      nftId = window.location.pathname.split('/').pop();
-      console.log('found nftId', nftId);
 
-      xrpAddress = await getXrpAddress(nftId);
-      console.log('found xrpAddress', xrpAddress);
-    } else if (site.url === 'https://xmagnetic.org') {
-      nftId = window.location.pathname.split('/').pop().split('?')[0];
-      console.log('found nftId', nftId);
-
-      xrpAddress = await getXrpAddress(nftId);
-      console.log('found xrpAddress', xrpAddress);
-    } else if (site.url === 'https://dexscreener.com/xrpl') {
-      await delay(2000);  // Bypass button insertion blocking due to ssr and csr mismatch
-      xrpAddress = window.location.pathname.split('/').pop().split('.').pop().split('_')[0];
-      console.log('Extracted XRP address from URL:', xrpAddress);
-    } else if (site.url === 'https://xpmarket.com/dex') {
-      xrpAddress = window.location.pathname.split('-').pop().split('/')[0];
-      console.log('Extracted XRP address from URL:', xrpAddress);
-    } else {
-      xrpAddress = findXRPAddressInNode(container);
+    if (site.pageLoadDelay) {
+      await delay(site.pageLoadDelay);
     }
+
+    if (site.addressInUrl) {
+      console.log('Extracting XRP address from URL:', site.addressInUrl);
+      xrpAddress = parseAndExecuteExpression(site.addressInUrl);
+      console.log('Extracted XRP address from URL:', xrpAddress);
+      if (Array.isArray(xrpAddress)) {
+        xrpAddress = findFirstXRPAddressInArray(xrpAddress);
+      }
+      console.log('Extracted XRP address from URL:', xrpAddress);
+    } else if (site.addressInNftId) {
+      if (site.addressInNftId === 'href') {
+        const hrefContainer = await waitForElement(site.selector);
+        console.log('Found href container:', hrefContainer);
+        nftId = hrefContainer.href.split('/').pop();
+        xrpAddress = await getXrpAddress(nftId);
+        console.log('Found XRP address:', xrpAddress);
+      } else {
+        nftId = parseAndExecuteExpression(site.addressInNftId);
+        console.log(`Extracted NFT ID: ${nftId}`);
+        if (nftId) {
+          nftId = String(nftId);
+          xrpAddress = await getXrpAddress(nftId);
+          console.log('Found XRP address:', xrpAddress);
+        } else {
+          console.error('NFT ID not found in URL path');
+        }
+      }
+    } else if (site.noXRPAddressInPath && site.buttonExists) {
+      try {
+        container = await waitForElement(site.selector, 5000);
+        console.log('Found Owner address container:', container);
+        foundXRPAddress = findXRPAddresses(container.textContent);
+        if (!foundXRPAddress) {
+          throw new Error('XRP address not found in node:', site.selector, 'on URL:', site.url);
+        }
+        xrpAddress = findXRPAddressInNode(container);
+      } catch (error) {
+        console.error(`Primary selector not found or timed out: ${site.selector}`);
+        secContainer = await waitForElement(site.secSelector, 5000);
+        console.log('Found secondary Owner address container:', secContainer);
+        xrpAddress = findXRPAddressInNode(secContainer);
+      }
+    } else {
+      container = await waitForElement(site.selector);
+      console.log('Found Owner address container:', container);
+      xrpAddress = findXRPAddressInNode(container);
+      console.log('Found XRP address:', xrpAddress);
+    }
+
 
     if (xrpAddress) {
       console.log('Found XRP address:', xrpAddress);
@@ -284,17 +322,30 @@ async function insertButtonForSite(site) {
         buttonText = 'Chat with wallet';
       }
       if (site.type === 'token') {
-        buttonText = 'chat with token issuer';
+        buttonText = 'Chat with token issuer';
+      }
+
+      // Remove the button if it already exists
+      if (site.secInsertSelector && site.buttonExists) {
+        const existingButton = document.querySelector('.contact-nft-owner-button');
+        if (existingButton) {
+          existingButton.remove();
+          console.log('Existing button removed:', existingButton);
+        }
       }
 
       const button = createButton(xrpAddress, buttonText);
-      await insertButton(site, button, site.url);
+      await insertButton(site, button, site.buttonAdjust);
       console.log('Button inserted:', button);
     } else {
       console.log('XRP address not found in node:', site.selector, 'on URL:', site.url);
     }
   } catch (error) {
-    console.error(error.message);
+    if (error.message.includes('Extension context invalidated')) {
+      console.warn('Extension context invalidated. Aborting operation.');
+    } else {
+      console.error(error.message);
+    }
   }
 }
 
@@ -318,6 +369,7 @@ function observeDynamicContent(site) {
 }
 
 async function checkAndInsertButton() {
+  console.log('checkAndInsertButton called');
   const currentUrl = window.location.href;
   const sites = await loadSitesConfig();
 
@@ -335,30 +387,30 @@ async function checkAndInsertButton() {
 }
 
 // Step-by-step debugging for Sologenic.org
-async function debugSelectors() {
-  const selectors = [
-    '#content-scroll',
-    '#content-scroll > div',
-    '#content-scroll > div > div.nft-container',
-    '#content-scroll > div > div.nft-container > div.left.top',
-    '#content-scroll > div > div.nft-container > div.left.top > div'
-  ];
+// async function debugSelectors() {
+//   const selectors = [
+//     '#content-scroll',
+//     '#content-scroll > div',
+//     '#content-scroll > div > div.nft-container',
+//     '#content-scroll > div > div.nft-container > div.left.top',
+//     '#content-scroll > div > div.nft-container > div.left.top > div'
+//   ];
 
-  for (const selector of selectors) {
-    try {
-      const element = await waitForElement(selector);
-      console.log(`Found element for selector: ${selector}`, element);
-    } catch (error) {
-      console.error(error.message);
-      break; // Stop if any selector fails
-    }
-  }
-}
+//   for (const selector of selectors) {
+//     try {
+//       const element = await waitForElement(selector);
+//       console.log(`Found element for selector: ${selector}`, element);
+//     } catch (error) {
+//       console.error(error.message);
+//       break; // Stop if any selector fails
+//     }
+//   }
+// }
 
 window.addEventListener('load', () => {
   injectStyles();
   checkAndInsertButton();
-  debugSelectors(); // Debugging for Sologenic.org
+  // debugSelectors(); // Debugging for Sologenic.org
 });
 
 // Monitor URL changes and re-run checkAndInsertButton
@@ -367,6 +419,7 @@ new MutationObserver(() => {
   const currentUrl = location.href;
   if (currentUrl !== lastUrl) {
     lastUrl = currentUrl;
-    setTimeout(checkAndInsertButton, 1000); // Allow time for the new page to load
+    setTimeout(checkAndInsertButton, 1500); // Allow time for the new page to load
+    console.log('URL changed:', currentUrl);
   }
 }).observe(document, { subtree: true, childList: true });
